fe7bf5a15100218f2cae6856eae0179a42796270
MxVaibs <mvaibhav.main@gmail.com>
2025-06-23 18:22:19 +0530

    Deleted existing files

--- Backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { register, login } = require('../controllers/authControl')
-const { log } = require('console')
-const router = express.Router()
-
-router.post('/register', register)
-router.post('/login', login)
-
-module.exports = router
